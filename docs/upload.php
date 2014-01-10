b40f2dccd5038454e1368c4e93333027f5e12e4b
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2014-01-10 23:34:26 +0100

    implemented upload-password and sourceid

--- docs/upload.php
+++ docs/upload.php
@@ -1,8 +1,11 @@
 <?php
+if($_GET['key'] != 'toTri7WnjrNoQeNi5kUssW')
+  die("wrong transaction-key");
+
 $postdata = file_get_contents("php://input");
 //$postdata = file_get_contents("php://stdin");
 
 include("database.php");
-$db->addUploadToDatabase(0, $postdata);
+$db->addUploadToDatabase($_GET['id'], $postdata);
 
 echo "done";
