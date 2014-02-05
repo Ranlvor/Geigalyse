937216fce6665df5d209373299c81a5b892a5f97
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2014-02-05 16:44:52 +0100

    Added index.php

---
+++ docs/index.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>Geigercounter-Status</title>
+</head>
+<body>
+<h1>Geigercounter-Status</h1>
+<h2>Latest Mesurement</h2>
+<table>
+<?php
+include("../analyse/populateMesurements.php");
+$array = $db->getLatestMesurementExtendet();
+$table['timestamp'] = date("d.m.y H:i:s", $array['timestamp'] - 2208988800);
+$table['Counts in the previous 60 seconds'] = $array['count'];
+$table['Calculated radiation'] = $array['mysvph']." µSv/h";
+$table['Used Geiber tube'] = $array['name'];
+$table['Used counts/minute to µSv/h-divisor'] = $array['cpm-per-mysvph']. " (counts/minute)/(µSv/h)";
+$table['Used deadtime'] = $array['deadtimeS'].' s';
+foreach($table as $key => $value) {
+  echo "<tr><td>$key</td><td>$value</td></tr>\n";
+}
+?>
+</table>
+<h2>Older Data</h2>
+<img src="drawChart.php" />
+</body>
