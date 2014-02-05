810272653277465b28a213b3b20ec74877079bf4
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2014-02-06 00:51:15 +0100

    Added piwik-tracking

--- docs/index.php
+++ docs/index.php
@@ -33,4 +33,19 @@ foreach($table as $key => $value) {
 </table>
 <h2>Older Data</h2>
 <img src="drawChart.php" />
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+
+  (function() {
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://piwik.starletp9.de/";
+    _paq.push(["setTrackerUrl", u+"piwik.php"]);
+    _paq.push(["setSiteId", "5"]);
+    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Piwik Code -->
 </body>
