f580b9cd38b39686d6c00ae8a21a08605d7a931c
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2014-02-10 19:29:24 +0100

    Added png-comment to gnuplot-graphs

--- docs/drawChart-week-gnuplot.php
+++ docs/drawChart-week-gnuplot.php
@@ -4,4 +4,35 @@ header("Content-Type: image/png");
 include("../analyse/populateMesurements.php");
 $db->populateSlidingAverageCache(1440*7, 3600);
 $db->populateSlidingAverageCache(1440*7, 300);
-passthru ("../analyse/plot-weekly.sh");
+$handle = popen ("../analyse/plot-weekly.sh", "r");
+$img = new Imagick();
+$img->readImageFile($handle);
+pclose($handle);
+
+$array = $db->getLatestMesurementExtendet();
+$table['Timestamp'] = date("d.m.y H:i:s", $array['timestamp'] - 2208988800);
+$table['Counts in the previous 60 seconds'] = $array['count'];
+$table['Calculated radiation'] = $array['mysvph']." µSv/h";
+$table['Used Geiger tube'] = $array['name'];
+$table['Used counts/minute to µSv/h-divisor'] = $array['cpm-per-mysvph']. " (counts/minute)/(µSv/h)";
+$table['Used deadtime'] = $array['deadtimeS'].' s';
+
+$average = $db->getLatestProcessedMesurementsSlidingAverage(1,5*60);
+$table['5 minute average'] = $average->fetchArray()['slidingAVG']." µSv/h";
+$average->finalize();
+
+$average = $db->getLatestProcessedMesurementsSlidingAverage(1,60*60);
+$table['60 minute average'] = $average->fetchArray()['slidingAVG']." µSv/h";
+$average->finalize();
+
+$comment = 'Latest Data available at http://geigalyse.starletp9.de/'."\n";
+$comment .= "\n";
+$comment .= "Information about latest measurement:\n";
+
+foreach($table as $key => $value) {
+  $comment .= "$key: $value\n";
+}
+
+
+$img->commentImage($comment);
+echo $img;
