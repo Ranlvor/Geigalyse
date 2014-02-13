6d133c142d264b198485370d7c7ad06464a14e6d
Kevin Kirsch <keine.ahnung.ka@googlemail.com>
2014-02-13 20:54:39 +0100

    added reprocess-uploads-script

---
+++ analyse/reprocess-uploads.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+echo "Resetting database";
+cd ../db
+sqlite3 uploads.db <<EOS
+BEGIN;
+UPDATE uploads SET processed = 0;
+UPDATE uploadsGet SET processed = 0;
+COMMIT;
+EOS
+
+echo "Reprocessing uploads";
+cd ../analyse
+php populateMesurements.php
+
+echo "discarding bad data"
+php <<EOS
+<?php
+include("../docs/database.php");
+\$db->applyBadTimestampTimes();
+EOS
+
+cd ../db
+echo "deleting sliding average cache (just to be sure)"
+sqlite3 results.db <<EOS
+DELETE FROM slidingaveragecache;
+EOS
+
+cd ../analyse
+echo "regenerating slidingaveragecache..."
+echo -n "regenerating last day..."
+./plot-dayly.sh > /dev/null && echo "[OK]" || echo "[FAIL]"
+echo -n "regenerating last week..."
+./plot-weekly.sh > /dev/null && echo "[OK]" || echo "[FAIL]"
+echo -n "regenerating last month..."
+./plot-monthly.sh > /dev/null && echo "[OK]" || echo "[FAIL]"
